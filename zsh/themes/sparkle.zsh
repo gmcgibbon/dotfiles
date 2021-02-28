a10e2ee2a31ed7ac61ecfd2bb8781df0607de491
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2021-02-28 10:02:14 -0500

    Remove hostname from sparkle theme

--- zsh/themes/sparkle.zsh
+++ zsh/themes/sparkle.zsh
@@ -2,20 +2,19 @@
 
 function ssh_connection() {
   if [[ -n $SSH_CONNECTION ]]; then
-    echo "%{$fg_bold[black]%}(ssh) "
+    echo "%{$fg_bold[magenta]%}(ssh) "
   fi
 }
 
 PROMPT='$(ssh_connection)'
-PROMPT+='%{${fg_bold[red]}%}%m '
 PROMPT+='✨ '
-PROMPT+='%{${fg_bold[magenta]}%}%c '
+PROMPT+='%{${fg_bold[yellow]}%}%c '
 PROMPT+='$(git_prompt_info)'
-PROMPT+='%{${fg_bold[cyan]}%}[%?] '
+PROMPT+='%{${fg_bold[grey]}%}[%?] '
 PROMPT+='%{${fg_bold[white]}%}%#'
 PROMPT+='%{${reset_color}%} '
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[yellow]%}<"
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[red]%}<"
 ZSH_THEME_GIT_PROMPT_SUFFIX="> %{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY=":✘"
 ZSH_THEME_GIT_PROMPT_CLEAN=":✔"
